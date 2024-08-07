706161c44d1115e6d2fcf2c1debc8efc759e8015
Pinyu Su <pinyusu@amazon.com>
2024-08-07 15:07:36 +0800

    add key ID

--- application/pages/7_📚_Entity_Management.py
+++ application/pages/7_📚_Entity_Management.py
@@ -134,7 +134,7 @@ def main():
                 st.write("This page support CSV or Excel files batch insert entity samples.")
                 st.write("**The Column Name need contain 'entity' and 'comment'**")
                 uploaded_files = st.file_uploader("Choose CSV or Excel files", accept_multiple_files=True,
-                                              type=['csv', 'xls', 'xlsx'])
+                                              type=['csv', 'xls', 'xlsx'], key="add metrics value")
                 if uploaded_files:
                     for i, uploaded_file in enumerate(uploaded_files):
                         status_text = st.empty()
@@ -158,7 +158,7 @@ def main():
                 st.write("This page support CSV or Excel files batch insert dimension entity samples.")
                 st.write("**The Column Name need contain 'entity' 'table' 'column' 'value' **")
                 uploaded_files = st.file_uploader("Choose CSV or Excel files", accept_multiple_files=True,
-                                                  type=['csv', 'xls', 'xlsx'])
+                                                  type=['csv', 'xls', 'xlsx'], key="add dimension value")
                 if uploaded_files:
                     for i, uploaded_file in enumerate(uploaded_files):
                         status_text = st.empty()
