3c57c928a2160396eb32ef2db1d4803dc6f9042d
Pinyu Su <pinyusu@amazon.com>
2024-08-07 15:17:48 +0800

    change entity comment

--- application/pages/7_📚_Entity_Management.py
+++ application/pages/7_📚_Entity_Management.py
@@ -156,7 +156,7 @@ def main():
         with batch_dimension_entity:
             if current_profile is not None:
                 st.write("This page support CSV or Excel files batch insert dimension entity samples.")
-                st.write("**The Column Name need contain 'entity' 'table' 'column' 'value' **")
+                st.write("**The Column Name need contain 'entity' 'table' 'column' 'value'**")
                 uploaded_files = st.file_uploader("Choose CSV or Excel files", accept_multiple_files=True,
                                                   type=['csv', 'xls', 'xlsx'], key="add dimension value")
                 if uploaded_files:
